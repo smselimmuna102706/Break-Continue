c394ce36f484e2f82924a79c07fdb7ba04593716
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-11 19:54:54 +0600

    Break-Continue

---
+++ 22.js
@@ -0,0 +1,21 @@
+
+
+for( var i = 1; i<=100; i++){
+    if(i==10){
+       break;
+    }
+    document.write("</br>"+ i);
+}
+
+document.write("</br>"+"end");
+
+
+for( var i = 1; i<=100; i++){
+    if(i==10){
+       continue;
+    }
+    document.write("</br>"+ i);
+}
+
+document.write("</br>"+"end");
+
